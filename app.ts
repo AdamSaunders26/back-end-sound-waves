cdf658f7bb5ac78139aceb9d2a643601b5a77a84
Valentina <loghin_valentina@yahoo.com>
2023-08-03 10:56:15 +0100

    resolved conflicts

--- app.ts
+++ app.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { getWaveById, getWaves, storeWave, getCommentsByWaveId, getBoards } from "./db/controllers/waves.controller";
-import cors from 'cors';
-import multer from 'multer';
+import {
+  getWaveById,
+  getWaves,
+  storeWave,
+} from "./db/controllers/waves.controller";
+import cors from "cors";
+import multer from "multer";
+import { getBoards } from "./db/controllers/boards.controller";
+import { getCommentsByWaveId } from "./db/controllers/comments.controller";
 
 dotenv.config();
 
 const app: Express = express();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use(express.json());
@@ -16,10 +22,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.get('/api/waves', getWaves);
-app.post('/api/waves', upload.single('audio_file'), storeWave);
+app.get("/api/waves", getWaves);
 
-app.get('/api/boards', getBoards);
+app.post("/api/waves", upload.single("audio_file"), storeWave);
+
+app.get("/api/boards", getBoards);
 
 app.get("/api/waves/:wave_id/comments", getCommentsByWaveId);
 
--- db/models/waves.model.ts
+++ db/models/waves.model.ts
@@ -2,35 +2,41 @@ import { Wave } from "../types/soundwaves-types";
 const db = require("../connection");
 
 export async function selectWaves(): Promise<Wave[]> {
-  const { rows }: {rows: Wave[]} = await db.query('SELECT * FROM waves;')
-  return rows
-}
-
-export const insertWave = ({
-  title,
-  username,
-  board_slug
-}: {
-  title: string;
-  username: string;
-  board_slug: string;
-}, wave_url: string): Promise<Wave> => {
-  console.log({ title, username, board_slug });
-  return db.query(`
-    INSERT INTO waves
-      (title, username, board_slug, wave_url)
-    VALUES
-      ($1, $2, $3, $4)
-    RETURNING *;
-  `, [title, username, board_slug, wave_url]);
-};
-
-const waves_query = `SELECT wave_id(waves), title(waves), wave_url(waves), created_at(waves), username(waves), board_name(boards), transcript  (waves), censor(waves), likes(waves), board_slug (boards), COUNT(comment_id(comments)) AS comment_count
+  const waves_query = `SELECT wave_id(waves), title(waves), wave_url(waves), created_at(waves), username(waves), board_name(boards), transcript (waves), censor(waves), likes(waves), board_slug(boards), COUNT(comment_id(comments)) AS comment_count
  FROM waves 
  LEFT JOIN comments ON wave_id(waves)= wave_id(comments)
  LEFT JOIN boards ON board_name(boards) = board_name(waves)
  GROUP BY wave_id(waves), wave_id(comments), board_name(boards) 
  ORDER BY wave_id(waves) DESC;`;
+
+  const { rows }: { rows: Wave[] } = await db.query(waves_query);
+  return rows;
+}
+
+export const insertWave = (
+  {
+    title,
+    username,
+    board_slug,
+  }: {
+    title: string;
+    username: string;
+    board_slug: string;
+  },
+  wave_url: string
+): Promise<Wave> => {
+  console.log({ title, username, board_slug });
+  return db.query(
+    `
+    INSERT INTO waves
+      (title, username, board_slug, wave_url)
+    VALUES
+      ($1, $2, $3, $4)
+    RETURNING *;
+  `,
+    [title, username, board_slug, wave_url]
+  );
+};
 
 export async function selectWaveById(wave_id: string): Promise<Wave> {
   const wave_query = `SELECT * FROM waves WHERE wave_id = $1;`;

e57a711aa50e7efce812fc8660e9ccd75fa131ec
Marija Kocevska <kocevska_m@hotmail.com>
2023-08-03 12:01:56 +0100

    change boards primary key to boards_slug

--- db/models/waves.model.ts
+++ db/models/waves.model.ts
@@ -5,8 +5,8 @@ export async function selectWaves(): Promise<Wave[]> {
   const waves_query = `SELECT wave_id(waves), title(waves), wave_url(waves), created_at(waves), username(waves), board_name(boards), transcript (waves), censor(waves), likes(waves), board_slug(boards), COUNT(comment_id(comments)) AS comment_count
  FROM waves 
  LEFT JOIN comments ON wave_id(waves)= wave_id(comments)
- LEFT JOIN boards ON board_name(boards) = board_name(waves)
- GROUP BY wave_id(waves), wave_id(comments), board_name(boards) 
+ LEFT JOIN boards ON board_slug(boards) = board_slug(waves)
+ GROUP BY wave_id(waves), wave_id(comments), board_slug(boards) 
  ORDER BY wave_id(waves) DESC;`;
 
   const { rows }: { rows: Wave[] } = await db.query(waves_query);
--- db/test-data/comments.ts
+++ db/test-data/comments.ts
@@ -1,53 +1,53 @@
 module.exports = [{
     "comment": "Expanded web-enabled leverage",
     "created_at": "2023-04-05T05:59:57Z",
-    "username": "Big C",
+    "username": "BigC",
   
     "wave_id": 6
   }, {
     "comment": "Assimilated web-enabled access",
     "created_at": "2023-06-24T06:49:52Z",
-    "username": "Big M",
+    "username": "BigM",
     "wave_id": 5
   }, {
     "comment": "Networked system-worthy circuit",
     "created_at": "2023-06-25T13:58:41Z",
-    "username": "Big A",
+    "username": "BigA",
     "wave_id": 1
   }, {
     "comment": "Fully-configurable context-sensitive approach",
     "created_at": "2022-10-07T12:55:50Z",
-    "username": "Big J",
+    "username": "BigJ",
     "wave_id": 10
   }, {
     "comment": "Open-architected 6th generation product",
     "created_at": "2022-09-21T21:28:42Z",
-    "username": "Big V",
+    "username": "BigV",
     "wave_id": 1
   }, {
     "comment": "Exclusive regional system engine",
     "created_at": "2022-12-06T17:35:51Z",
-    "username": "Big C",
+    "username": "BigC",
     "wave_id": 8
   }, {
     "comment": "Synergized value-added application",
     "created_at": "2023-04-23T18:27:55Z",
-    "username": "Big V",
+    "username": "BigV",
     "wave_id": 9
   }, {
     "comment": "Synchronised non-volatile middleware",
     "created_at": "2023-05-15T04:31:13Z",
-    "username": "Big M",
+    "username": "BigM",
     "wave_id": 3
   }, {
     "comment": "Cross-platform composite neural-net",
     "created_at": "2023-07-20T21:05:12Z",
-    "username": "Big A",
+    "username": "BigA",
     "wave_id": 3
   }, {
     "comment": "Realigned 24 hour capacity",
     "created_at": "2023-04-23T19:29:52Z",
-    "username": "Big J",
+    "username": "BigJ",
     "wave_id": 6
   }]
   
